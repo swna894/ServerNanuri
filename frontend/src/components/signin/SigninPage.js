9af785dde1127152a728de7740eafffede0071e4
Juno <Juno@Juno>
2021-04-17 22:33:42 +0900

    SigninPage Header / Footer 추가

--- frontend/src/components/signin/SigninPage.js
+++ frontend/src/components/signin/SigninPage.js
@@ -4,11 +4,12 @@ import { Form, Input, Button, Checkbox, Row, Col, Card } from "antd";
 import { UserOutlined, LockOutlined } from "@ant-design/icons";
 import { useDispatch } from "react-redux";
 import { loginUser } from "../../_actions/user_action";
+import { Layout } from "antd";
 import "./SignInPage.css";
 
 function SignPagePage(props) {
   const dispatch = useDispatch();
-
+  const { Header } = Layout;
   const [Email, setEmail] = useState("");
   const [Password, setPassword] = useState("");
 
@@ -48,78 +49,102 @@ function SignPagePage(props) {
   };
 
   return (
-    <Row
-      type="flex"
-      justify="center"
-      align="middle"
-      style={{
-        height: "100vh",
-      }}
-    >
-      <Col>
-        <Card className="login-form-card">
-          <Form
-            name="normal_login"
-            className="login-form"
-            initialValues={{ remember: true }}
-            onSubmit={onSubmitSingin}
-          >
-            <Form.Item
-              name="Email"
-              rules={[{ required: true, message: "Please input your Email!" }]}
+    <div style={{ backgroundColor: "#f0f0f0" }}>
+      <Header
+        style={{
+          position: "fixed",
+          zIndex: 1,
+          width: "100%",
+        }}
+      >
+        <h2 style={{ color: "#fff" }}>Welcome to David Na's Ordering System</h2>
+      </Header>
+      <Row
+        type="flex"
+        justify="center"
+        align="middle"
+        style={{
+          height: "calc(100vh - 65px)",
+         }}
+      >
+        <Col>
+          <Card className="login-form-card">
+            <Form
+              name="normal_login"
+              className="login-form"
+              initialValues={{ remember: true }}
+              onSubmit={onSubmitSingin}
             >
-              <Input
-                type="email"
-                value={Email}
-                onChange={onChangeEmail}
-                prefix={<UserOutlined className="site-form-item-icon" />}
-                placeholder="Email"
-              />
-            </Form.Item>
-            <Form.Item
-              name="password"
-              rules={[
-                { required: true, message: "Please input your Password!" },
-                {
-                  required: true,
-                  min: 3,
-                  message: "Password must be minimum 3 characters.",
-                },
-              ]}
-            >
-              <Input
-                type="password"
-                value={Email}
-                onChange={onChangePassword}
-                prefix={<LockOutlined className="site-form-item-icon" />}
-                placeholder="Password"
-              />
-            </Form.Item>
-            <Form.Item>
-              <Form.Item name="remember" valuePropName="" noStyle>
-                <Checkbox>Remember me</Checkbox>
+              <Form.Item
+                name="Email"
+                rules={[
+                  { required: true, message: "Please input your Email!" },
+                ]}
+              >
+                <Input
+                  type="email"
+                  value={Email}
+                  onChange={onChangeEmail}
+                  prefix={<UserOutlined className="site-form-item-icon" />}
+                  placeholder="Email"
+                />
+              </Form.Item>
+              <Form.Item
+                name="password"
+                rules={[
+                  { required: true, message: "Please input your Password!" },
+                  {
+                    required: true,
+                    min: 3,
+                    message: "Password must be minimum 3 characters.",
+                  },
+                ]}
+              >
+                <Input
+                  type="password"
+                  value={Email}
+                  onChange={onChangePassword}
+                  prefix={<LockOutlined className="site-form-item-icon" />}
+                  placeholder="Password"
+                />
+              </Form.Item>
+              <Form.Item>
+                <Form.Item name="remember" valuePropName="" noStyle>
+                  <Checkbox>Remember me</Checkbox>
+                </Form.Item>
+
+                <Link className="login-form-forgot" to="/signin">
+                  Forgot password
+                </Link>
               </Form.Item>
 
-              <Link className="login-form-forgot" to="/signin">
-                Forgot password
-              </Link>
-            </Form.Item>
-
-            <Form.Item>
-              <Button
-                type="primary"
-                htmlType="submit"
-                className="login-form-button"
-                onClick={onClickSingin}
-              >
-                Sign in
-              </Button>
-              <Link to="/signup">Or register now!</Link>
-            </Form.Item>
-          </Form>
-        </Card>
-      </Col>
-    </Row>
+              <Form.Item>
+                <Button
+                  type="primary"
+                  htmlType="submit"
+                  className="login-form-button"
+                  onClick={onClickSingin}
+                >
+                  Sign in
+                </Button>
+                <Link to="/signup">Or register now!</Link>
+              </Form.Item>
+            </Form>
+          </Card>
+        </Col>
+      </Row>
+      <Header
+        style={{
+          position: "sticky",
+          zIndex: 1,
+          width: "100%",
+        }}
+      >
+        <h2 style={{ color: "#fff" }}>
+          Enquiry ? Please Contact : David Na 027-652-1111
+        </h2>
+      </Header>
+    </div>
   );
 }
 
