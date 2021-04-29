5ae360a8b87f46ba8e5ed8b36c1587b4f27c747f
Juno <Juno@Juno>
2021-04-29 17:57:30 +0900

    responsive Footer

--- frontend/src/components/ordering/OrderFooter.js
+++ frontend/src/components/ordering/OrderFooter.js
@@ -2,13 +2,14 @@ import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Layout, Pagination } from "antd";
 import { getProductsAction } from "../../_actions/product_action";
+import { useWindowWidthAndHeight } from "../../utils/CustomHooks";
 import "./OrderPage.css";
 
 function OrderFooter() {
   const supplier = useSelector((state) => state.supplier.supplier);
   const current = useSelector((state) => state.product.products.number);
   const category = useSelector((state) => state.supplier.category);
-  //const categories = useSelector((state) => state.product.categories);
+
   const { Footer } = Layout;
   const dispatch = useDispatch();
   const totalElements = useSelector(
@@ -28,30 +29,42 @@ function OrderFooter() {
     //console.log("Page: ", pageNumber-1);
     //console.log("pageSize: ", pageSize);
   }
-const footerStyel = {
-             position: "fixed",
-          width: "100%",
-          bottom: "0",
-          paddingTop: 10,
-          paddingBottom: 10,
-          textAlign: "right",
-          backgroundColor: "#bfbfbf",
+
+  const [width] = useWindowWidthAndHeight();
+
+  const footerStyel = {
+    position: "fixed",
+    width: "100%",
+    bottom: "0",
+    paddingTop: 10,
+    paddingBottom: 10,
+    textAlign: "right",
+    backgroundColor: "#bfbfbf",
   };
+
   return (
     <div>
       <Footer style={footerStyel}>
-        <Pagination
-          current={current + 1}
-          defaultPageSize={36}
-          pageSizeOptions={[16, 24, 36, 60, 100]}
-          showSizeChanger
-          howSizeChanger={true}
-          showQuickJumper
-          defaultCurrent={1}
-          total={totalElements}
-          onChange={onChange}
-          showTotal={(total) => `Total ${total} items`}
-        />
+        {width > 1000 ? (
+          <Pagination
+            current={current + 1}
+            defaultPageSize={36}
+            pageSizeOptions={[16, 24, 36, 60, 100]}
+            showSizeChanger
+            howSizeChanger={true}
+            showQuickJumper
+            defaultCurrent={1}
+            total={totalElements}
+            onChange={onChange}
+            showTotal={(total) => `Total ${total} items`}
+          />
+        ) : (
+          <Pagination
+            total={totalElements}
+            defaultPageSize={36}
+            defaultCurrent={1}
+          />
+        )}
       </Footer>
     </div>
   );
