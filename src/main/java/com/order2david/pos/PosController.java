3a17236f06997cec8721eeb877014887d2645de9
DESKTOP-MOHPV7O\우리집 <swna8934@gmail.com>
2021-09-20 19:34:12 +0900

    PosSystem order uploading 추가

--- src/main/java/com/order2david/pos/PosController.java
+++ src/main/java/com/order2david/pos/PosController.java
@@ -52,14 +52,17 @@ public class PosController {
 
 	@PostMapping("/orderItems")
 	public List<OrderItem> postOrderItems(@RequestBody List<OrderItem> orderItems) {
-		
 		String abbr = orderItems.get(0).getAbbr();
 		String shopId = orderItems.get(0).getShopId();
 		String invoice = abbr + shopId + "_CART"; 
+		
 		Shop shop = shopRepository.findByAbbr(orderItems.get(0).getShopId());
 		Supplier supplier = supplierRepository.findByAbbr(abbr);
 		
 		orderItems = checkIsStock(orderItems, invoice);
+	    if(orderItems.isEmpty()) {
+	    	return null;
+	    }
 		Optional<Order> orderOptional = orderRepository.findByInvoice(invoice);
 		Order order;
 		if (!orderOptional.isPresent()) {  // order가 없으면
